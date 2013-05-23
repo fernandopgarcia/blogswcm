97178c8b9661382b93479e3a774c090915c7b134
Fernando Garcia <fernando.teleco@hotmail.com>
2013-05-23 12:35:10 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,9 @@
-
 /*
  * GET home page.
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express');
+  res.render('index', { title: 'Express'});
 };
 exports.video = function(req, res){
   res.render('video', { title: 'Express' });
